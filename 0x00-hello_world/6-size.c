2909df2ac83d106c3af2a6b72e29ab319d6f2735
TshepoMamakoko <mamakokotshepo@outlook.com>
2023-03-19 02:45:27 -0700

    number 6 test run trail 4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,20 +9,20 @@
 
 int main(void)
 {
-	char = 1
+	char = 1;
 
-	int = 4
+	int = 4;
 
-	long int = 4
+	long int = 4;
 
-	long long int = 8
+	long long int = 8;
 
-	float = 4
+	float = 4;
 
-	printf("Size of a char: %c bytes(s)\n", char);
-	printf("Size of an int: %d byte(s)\n ", int);
-	printf("Size of a long int: %d byte(s)\n ", long int);
-	printf("Size of a long long int: %d byte(s)\n ", long long int);
-	printf("Size of a float: %f byte(s)\n", float);
+	printf("Size of a char: %c bytes(s)\n"), char;
+	printf("Size of an int: %d byte(s)\n "), int;
+	printf("Size of a long int: %d byte(s)\n ", long int;
+	printf("Size of a long long int: %d byte(s)\n "), long long int;
+	printf("Size of a float: %f byte(s)\n"), float;
 	return (0);
 }
