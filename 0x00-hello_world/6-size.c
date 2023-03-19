c956f33c39cee9598507ff1e18e03dca25aa705c
TshepoMamakoko <mamakokotshepo@outlook.com>
2023-03-19 00:58:20 -0700

    checing code 6 even with warnings

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Description: 'a C program that prints the size of various types '
+ *
+ * return: (0)
+ */
+
+int main(void)
+{
+	char = 1
+
+	int = 4
+
+	long int = 4
+
+	long long int = 8
+
+	float = 4
+
+	printf("Size of a char: %c bytes(s)\n", char);
+	printf("Size of an int: %d byte(s)\n ", int);
+	printf("Size of a long int: %d byte(s)\n ", long int);
+	printf("Size of a long long int: %d byte(s)\n ", long long int);
+	printf("Size of a float: %f byte(s)\n", float);
+	return (0);
+}
