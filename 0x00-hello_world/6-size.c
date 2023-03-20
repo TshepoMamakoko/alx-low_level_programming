58f06708ad518cc08de83de2c2928b11cabcdadc
TshepoMamakoko <mamakokotshepo@outlook.com>
2023-03-20 09:24:08 -0700

    my 125 chance at , im crying

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,13 @@
 /*
  * main - Entry point
  *
- * Description: 'a C program that prints the size of various types '
+ * Description: 'a C program that prints the size of various types'
  *
  * return: Always 0 (Success)
  */
 
-int main(void);
+int main(void)
+
 {
 	char a;
 	int b;
@@ -15,10 +16,10 @@ int main(void);
 	long long int d;
 	float e;
 
-	printf("Size of a char: %i byte(s)\n", sizeof(char a));
-	printf("Size of an int: %i byte(s)\n", sizeof(int b));
-	printf("Size of a long int: %i byte(s)\n", sizeof(long int c));
-	printf("Size of a long long int: %i bytes(s)\n", sizeof(long long int d));
-	printf("Size of a float: %i bytes(s)\n", sizeof(float e));
+	printf("Size of a char: %i byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %i byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %i byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %i bytes(s)\n", (unsigned long) sizeof(e));
 	return (0);
 }
