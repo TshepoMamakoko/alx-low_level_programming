99d6ffb7a51c329fd608ad8503bb24e126e35f36
TshepoMamakoko <mamakokotshepo@outlook.com>
2023-03-20 07:21:30 -0700

    Question 6 78th attempt

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,18 @@
  * return: Always 0 (Success)
  */
 
-int main(char a, int b, long int c, long long int d, float e);
+int main(void)
 {
-	printf("Size of a char: %i byte(s)\n", sizeof(char));
-	printf("Size of an int: %i byte(s)\n", sizeof(int));
-	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %i bytes(s)\n", sizeof(long long int));
-	printf("Size of a float: %i bytes(s)\n", sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %i byte(s)\n", sizeof(char a));
+	printf("Size of an int: %i byte(s)\n", sizeof(int b));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int c));
+	printf("Size of a long long int: %i bytes(s)\n", sizeof(long long int d));
+	printf("Size of a float: %i bytes(s)\n", sizeof(float e));
 	return (0);
 }
