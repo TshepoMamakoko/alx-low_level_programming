3d6c6e9c82bbc8f77d37f815ed4caa4bf60970c1
TshepoMamakoko <mamakokotshepo@outlook.com>
2023-03-22 04:50:35 -0700

    small and big alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in lowercase, and then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOP";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
