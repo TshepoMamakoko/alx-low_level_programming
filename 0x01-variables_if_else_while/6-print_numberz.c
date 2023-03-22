5a30d994cbb58c0a659f3930e3e08f126d27d8a0
TshepoMamakoko <mamakokotshepo@outlook.com>
2023-03-22 08:44:57 -0700

    5th trail for number 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,13 +8,10 @@
  */
 int main(void)
 {
-	int i;
+	int d;
 
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d", i);
-	}
-	putchar(i);
+	for (d = '0'; d < '9'; d++)
+	putchar(d);
 	putchar('\n');
 	return (0);
 }
