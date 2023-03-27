cc18baab3c901e519087be93a4d6d40e14347099
TshepoMamakoko <mamakokotshepo@outlook.com>
2023-03-27 03:31:14 -0700

    this should check from 0 to 9

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,8 +11,7 @@
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 98)
-	{
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
 }
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,22 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
+ * _isdigit - that checks for a digit (0 through 9)
+ * @c: char to be checked
  *
- * Return: Always 0.
+ * Return: 0 or 1
  */
-int main(void)
-{
-    char c;
 
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
-    return (0);
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
-julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-isdigit.c -o 1-isdigit
-julien@ubuntu:~/0x04$ ./1-isdigit 
-0: 1
-a: 0
