a5479cdbb940bdda34d18128a6251ab1cbe8ac0a
TshepoMamakoko <mamakokotshepo@outlook.com>
2023-03-28 10:57:12 -0700

    a function that copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,7 +4,7 @@
  * _strcpy - copies the string
  * @dest: destination array
  * @src: source array
- * Return: copied string
+ * Return: the pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
@@ -14,6 +14,9 @@ char *_strcpy(char *dest, char *src)
 	if (dest == NULL)
 		return (NULL);
 	for (i = 0 ; src[i] != '\0' ; i++)
+	{
 		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
 	return (dest);
 }
