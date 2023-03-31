8f8a0659bd8daef55d9fd9a5301713d052104502
TshepoMamakoko <mamakokotshepo@outlook.com>
2023-03-30 20:05:08 -0700

    Write a function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,24 +3,21 @@
  * _strcmp - compare strings
  * @s1: strings
  * @s2: strings
- * Return: Always 0
+ * Return: s1[i] - s2[i]
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			if (s1[i] < s2[i])
-				return (s1[i] - s2[i]);
-			else if (s1[i] > s2[i])
-				return (s1[i] - s2[i]);
+			return (s1[i] - s2[i]);
 		}
-		else
-			return (0);
+		i++;
 	}
 	return (0);
 }
